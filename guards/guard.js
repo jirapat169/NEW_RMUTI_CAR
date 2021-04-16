65ca8f1a41a7ebc0d544cf3b8cd1999436c80b6c
jirapatsk <james.jirapat@gmail.com>
2021-04-16 18:59:16 +0700

    Update Guard

--- guards/guard.js
+++ guards/guard.js
@@ -1,17 +1,51 @@
 import React from "react";
 
-const Guard = (Components) => (props) => {
-  return (
-    <React.Fragment>
-      {(() => {
-        if (false) {
-          return <Components {...props}></Components>;
-        } else {
-          return <div>Not Found</div>;
-        }
-      })()}
-    </React.Fragment>
-  );
+const Guard = (props) => {
+  const AllowPath = ["signin", "register"];
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+  }, []);
+
+  if (loading)
+    return (
+      <React.Fragment>
+        {(() => {
+          if (
+            AllowPath.indexOf(
+              `${window.location.pathname.replace(/\//g, "")}`
+            ) != -1
+          ) {
+            return <>{props.children}</>;
+          } else {
+            if (props.isLogin) {
+              return <>{props.children}</>;
+            } else {
+              return (
+                <>
+                  <div className="spinner-border text-primary" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+
+                  {(() => {
+                    window.location.replace("/signin");
+                  })()}
+                </>
+              );
+            }
+          }
+        })()}
+      </React.Fragment>
+    );
+  else
+    return (
+      <React.Fragment>
+        <div className="spinner-border text-primary" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </React.Fragment>
+    );
 };
 
 export default Guard;
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import useLocalStorage from "../components/useLocalStorage";
 import { createMuiTheme } from "@material-ui/core/styles";
 import red from "@material-ui/core/colors/red";
+import Guard from "../guards/guard";
 
 const env = require(`../environments/${process.env.mode}`).default;
 const ThemeConfig = createMuiTheme({
@@ -38,18 +39,33 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ThemeProvider theme={ThemeConfig}>
-      <Component
+      <Guard
         {...pageProps}
         {...state}
         patchState={(e) => {
           patchState({ ...state, ...e });
         }}
         userLogin={Object.keys(userLogin).length > 0 ? userLogin : null}
+        isLogin={Object.keys(userLogin).length > 0 ? true : false}
         patchUserLogin={(e) => {
           patchUserLogin(e);
         }}
         env={env}
-      />
+      >
+        <Component
+          {...pageProps}
+          {...state}
+          patchState={(e) => {
+            patchState({ ...state, ...e });
+          }}
+          userLogin={Object.keys(userLogin).length > 0 ? userLogin : null}
+          isLogin={Object.keys(userLogin).length > 0 ? true : false}
+          patchUserLogin={(e) => {
+            patchUserLogin(e);
+          }}
+          env={env}
+        />
+      </Guard>
     </ThemeProvider>
   );
 }
